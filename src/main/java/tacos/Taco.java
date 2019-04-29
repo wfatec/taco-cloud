8f4702a04c7cbb38394cd054c121e863e19cbbb7
wangchao28 <wangchao28@vipkid.com.cn>
2019-04-29 21:15:44 +0800

    init

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,24 @@
+package tacos;
+
+import java.util.List;
+import java.util.Date;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Taco {
+
+	private Long id;
+	private Date createdAt;
+	// end::allButValidation[]
+	@NotNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
+	// tag::allButValidation[]
+	private String name;
+	// end::allButValidation[]
+	@Size(min = 1, message = "You must choose at least 1 ingredient")
+	// tag::allButValidation[]
+	private List<String> ingredients;
+
+}
