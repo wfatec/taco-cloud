df9b50072bbe2491fe510d377f4b7da2655bf82e
wfatec <chao125010@163.com>
2019-04-30 09:53:15 +0800

    delete unused code

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -34,24 +34,6 @@ public class DesignTacoController {
 		this.ingredientRepo = ingredientRepo;
 	}
 
-//	@GetMapping
-//	public String showDesignForm(Model model) {
-//		List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-//				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-//				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-//				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES), new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-//				new Ingredient("CHED", "Cheddar", Type.CHEESE), new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-//				new Ingredient("SLSA", "Salsa", Type.SAUCE), new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-//
-//		Type[] types = Ingredient.Type.values();
-//		for (Type type : types) {
-//			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
-//		}
-//
-//		model.addAttribute("design", new Taco());
-//		return "design";
-//
-//	}
 	@GetMapping
 	public String showDesignForm(Model model) {
 		List<Ingredient> ingredients = new ArrayList<>();
