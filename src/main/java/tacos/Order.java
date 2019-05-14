64ab885b58f0144816bbc1703d138a446d4a95ea
wangchao28 <wangchao28@vipkid.com.cn>
2019-05-14 20:33:04 +0800

    annotating the domain as entities

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,65 +1,60 @@
-//tag::all[]
-//tag::allButValidation[]
 package tacos;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-
+import javax.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
 import lombok.Data;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	private Date placedAt;
 
-	// end::allButValidation[]
 	@NotBlank(message = "Name is required")
-	// tag::allButValidation[]
 	private String name;
-	// end::allButValidation[]
 
 	@NotBlank(message = "Street is required")
-	// tag::allButValidation[]
 	private String street;
-	// end::allButValidation[]
 
 	@NotBlank(message = "City is required")
-	// tag::allButValidation[]
 	private String city;
-	// end::allButValidation[]
 
 	@NotBlank(message = "State is required")
-	// tag::allButValidation[]
 	private String state;
-	// end::allButValidation[]
 
 	@NotBlank(message = "Zip code is required")
-	// tag::allButValidation[]
 	private String zip;
-	// end::allButValidation[]
 
 	@CreditCardNumber(message = "Not a valid credit card number")
-	// tag::allButValidation[]
 	private String ccNumber;
-	// end::allButValidation[]
 
 	@Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
-	// tag::allButValidation[]
 	private String ccExpiration;
-	// end::allButValidation[]
 
 	@Digits(integer = 3, fraction = 0, message = "Invalid CVV")
-	// tag::allButValidation[]
 	private String ccCVV;
 
 	@ManyToMany(targetEntity = Taco.class)
@@ -67,6 +62,11 @@ public class Order {
 
 	public void addDesign(Taco design) {
 		this.tacos.add(design);
+	}
+
+	@PrePersist
+	void placedAt() {
+		this.placedAt = new Date();
 	}
 
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,27 +1,40 @@
 package tacos;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
 	private Date createdAt;
-	// end::allButValidation[]
+
 	@NotNull
 	@Size(min = 5, message = "Name must be at least 5 characters long")
-	// tag::allButValidation[]
 	private String name;
 
 	@ManyToMany(targetEntity = Ingredient.class)
 	@Size(min = 1, message = "You must choose at least 1 ingredient")
-	private List<Ingredient> ingredients = new ArrayList<>();
+	private List<Ingredient> ingredients;
+
+	@PrePersist
+	void createdAt() {
+		this.createdAt = new Date();
+	}
 
 }
