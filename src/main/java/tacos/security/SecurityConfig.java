0804f23f8169e67925cdca4655f87fcf8927362a
wangchao28 <wangchao28@vipkid.com.cn>
2019-05-15 19:47:31 +0800

    add login and logout

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,17 +1,45 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+	        .antMatchers("/design", "/orders")
+	          .access("hasRole('ROLE_USER')")
+	        .antMatchers("/", "/**").access("permitAll")
+		.and()
+	        .formLogin()
+	          .loginPage("/login")
+		.and()
+	        .logout()
+	          .logoutSuccessUrl("/");
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.ldapAuthentication().userSearchBase("ou=people").userSearchFilter("(uid={0})").groupSearchBase("ou=groups")
-				.groupSearchFilter("member={0}").passwordCompare();
+		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
 	}
 }
