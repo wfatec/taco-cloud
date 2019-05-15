c9406de4fbf0aa6b091c4872b1fd83208b3a40f1
wangchao28 <wangchao28@vipkid.com.cn>
2019-05-15 16:40:13 +0800

    jdbc based user store

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,26 +1,26 @@
 package tacos.security;
 
-import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-		.inMemoryAuthentication()
-			.withUser("buzz")
-				.password("infinity")
-					.authorities("ROLE_USER")
-		.and()
-		.withUser("woody")
-			.password("bullseye")
-				.authorities("ROLE_USER");
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from Users " + "where username=?")
+				.authoritiesByUsernameQuery("select username, authority from UserAuthorities " + "where username=?")
+				.passwordEncoder(new SCryptPasswordEncoder());
 	}
-
 }
